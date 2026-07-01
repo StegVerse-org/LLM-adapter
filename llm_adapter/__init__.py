1ed316ec187b8571c1310110cec22feed04ef8de
StegVerse <210078727+StegVerse@users.noreply.github.com>
2026-07-01 12:31:41 -0500

    Export provider and retrieval helpers

--- llm_adapter/__init__.py
+++ llm_adapter/__init__.py
@@ -2,14 +2,34 @@
 
 from .governed_adapter import (
     AdapterDecision,
+    EvidencePointer,
     GovernedAdapterResult,
     GovernedLLMAdapter,
     govern_response,
 )
+from .provider_request import (
+    ProviderMessage,
+    ProviderRequest,
+    build_provider_request,
+    normalize_messages,
+)
+from .retrieval_evidence import (
+    evidence_from_fixture,
+    evidence_from_payload,
+    evidence_list_from_fixtures,
+)
 
 __all__ = [
     "AdapterDecision",
+    "EvidencePointer",
     "GovernedAdapterResult",
     "GovernedLLMAdapter",
+    "ProviderMessage",
+    "ProviderRequest",
+    "build_provider_request",
+    "evidence_from_fixture",
+    "evidence_from_payload",
+    "evidence_list_from_fixtures",
     "govern_response",
+    "normalize_messages",
 ]
