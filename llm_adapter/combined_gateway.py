7900337790319f41ae07f3216297f344c30f0665
StegVerse <210078727+StegVerse@users.noreply.github.com>
2026-07-11 23:02:59 -0500

    mount External Chat router on governed gateway

---
+++ llm_adapter/combined_gateway.py
@@ -0,0 +1,5 @@
+"""Combined governed gateway application for Ecosystem Chat and External Chat."""
+from llm_adapter.ecosystem_chat_gateway import app
+from llm_adapter.external_chat_api import router as external_chat_router
+
+app.include_router(external_chat_router)
